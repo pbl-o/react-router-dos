e47cb5ef03197c9f4f60e51c430fd33f2efc581b
pblo <pdiazarenas541@gmail.com>
2025-10-20 03:05:09 +0100

    comment code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+/* import { useNavigate } from "react-router-dom"; */
 import { UserContext } from "../context/UserContext";
 
 const Login = () => {
